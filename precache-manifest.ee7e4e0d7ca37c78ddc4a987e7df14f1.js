874d2f49fb83c24cac131426bbd30371665548ac
nadeemkhan6993 <nadeemkhan6993@gmail.com>
2020-08-31 02:29:47 +0530

    Updates

---
+++ precache-manifest.ee7e4e0d7ca37c78ddc4a987e7df14f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3baec4f928187969584b7f5a09d16411",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "775d581d5cdbaec8d31a",
+    "url": "/covid-tracker/static/css/main.a4bbb776.chunk.css"
+  },
+  {
+    "revision": "b773b5f36c9b389b51f5",
+    "url": "/covid-tracker/static/js/2.3521f510.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/covid-tracker/static/js/2.3521f510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "775d581d5cdbaec8d31a",
+    "url": "/covid-tracker/static/js/main.afc6b57a.chunk.js"
+  },
+  {
+    "revision": "f33c9f5196452f979187",
+    "url": "/covid-tracker/static/js/runtime-main.d6c71d54.js"
+  },
+  {
+    "revision": "c45cf0e966a167ba0597346f8a7c061c",
+    "url": "/covid-tracker/static/media/coronavirus.c45cf0e9.jpg"
+  }
+]);
